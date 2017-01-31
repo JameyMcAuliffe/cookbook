ea5b2d04a56e6c15b3797e7b3d5e03d670eaa405
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2017-01-31 13:52:30 -0600

    added middlewares

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 'use strict'
 
 const express = require('express')
+let bodyParser = require('body-parser')
+const path = require('path')
+//const { Server } = require('http')
 
 const app = express()
 
 const port = process.env.PORT || 3000
 app.set('port', port)
+
+// app.get('/', function (req, res) {
+//   res.send('Cookbook')
+// })
+
+//View Engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+//Body Parser Middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+//Set Static Path
+app.use(express.static(path.join(__dirname, 'public')))
+
+
 
 app.listen(app.get('port'), () => {
 	console.log(`Express server listening on port ${port}`)
